9b5261ecdf6f73aa5b5cc00451111ffc07e122dd
vshchutc <victoria.schutska@gmail.com>
2023-03-15 18:59:46 +0100

    input validations

---
+++ src/input-validations.ts
@@ -0,0 +1,64 @@
+import { isValid, parse } from 'date-fns'
+
+// RegExp to check if a string can be parsed as a decimal number
+//
+// ^ - Start of line
+// \d* - 0 or more digits
+// \.? - An optional dot
+// \d* - 0 or more digits
+// $ - End of line
+
+const decimalCheck = /^\d*\.?\d*$/
+
+export function validateDate(dateString: string): Date {
+  try {
+    let date = parse(dateString, 'yyyy-MM-dd', new Date(2020, 1, 1))
+
+    if (!isValid(date)) {
+      throw `Date string "${dateString}" is not in the correct format. Please use yyyy-MM-dd`
+    }
+
+    return date
+  } catch (error) {
+    throw 'Could not parse date string. Please use yyyy-MM-dd'
+  }
+}
+
+export function validateStartAddress(start: string): string {
+  if (!start) throw `Starting address is not provided. Please enter an address`
+  return start
+}
+
+export function validateEndAddress(end: string): string {
+  if (!end) throw `Destination address is not provided. Please enter an address`
+  return end
+}
+
+export function validateCost(cost: string): number {
+  if (!cost) throw `Cost is not provided. Please enter the price of the ride`
+  
+  if (!decimalCheck.test(cost)) {
+    throw `Cost "${cost}" is not in the correct format. Please enter a number, with dot as a separator if needed.`
+  }
+
+  try {
+    return parseFloat(cost)
+  } catch (error) {
+    throw `Cost "${cost}" is not in the correct format. Please enter a number, with dot as a separator if needed.`
+  }
+}
+
+export function validateDistance(distance: string): number {
+  if (!distance) throw(`Please enter the distance of the ride`)
+
+  if (!decimalCheck.test(distance)) {
+    throw `Distance "${distance}" is not in the correct format. Please enter a number, with dot as a separator if needed.`
+  }
+
+  try {
+    return parseFloat(distance)
+  } catch (error) {
+    console.log('HERE')
+    throw `Distance "${distance}" is not in the correct format. Please enter a number, with dot as a separator if needed.`
+  }
+}
